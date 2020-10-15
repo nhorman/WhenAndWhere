2e34558f0908b4189e935ac2661f0d69ea7466e1
Neil Horman <nhorman@gmail.com>
2020-10-15 10:27:43 -0400

    Adding WhenWhere activity

--- app/src/main/java/com/thinkfreely/whenandwhere/WhenAndWhereActivity.kt
+++ app/src/main/java/com/thinkfreely/whenandwhere/WhenAndWhereActivity.kt
@@ -2,13 +2,11 @@ package com.thinkfreely.whenandwhere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 
@@ -16,34 +14,18 @@ import android.widget.TextView
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class GameSetupActivity : AppCompatActivity() {
-
-
+class WhenAndWhereActivity : AppCompatActivity() {
 
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_game_setup)
+        setContentView(R.layout.activity_when_and_where)
         supportActionBar?.hide()
-        val simple = findViewById(R.id.SimpleJustWhenButton) as ImageButton
-        simple.setOnClickListener {
-            val intent = Intent(this, SimpleJustWhenActivity::class.java)
-            startActivity(intent)
-        }
 
-        val whenwhere = findViewById(R.id.SingleWhenAndWhere) as ImageButton
-        whenwhere.setOnClickListener {
-            val intent = Intent(this, WhenAndWhereActivity::class.java)
-            startActivity(intent)
-        }
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
+
     }
-
-
 
 
     companion object {
