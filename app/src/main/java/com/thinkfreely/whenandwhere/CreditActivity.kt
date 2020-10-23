ad02c3db22ce1cb835dd5562ca7199e5e43e07d0
Neil Horman <nhorman@gmail.com>
2020-10-23 14:13:04 -0400

    fixing credit view

--- app/src/main/java/com/thinkfreely/whenandwhere/CreditActivity.kt
+++ app/src/main/java/com/thinkfreely/whenandwhere/CreditActivity.kt
@@ -30,16 +30,19 @@ class CreditActivity : AppCompatActivity() {
             scopeMainThread.launch {
                 val creditlayout = findViewById(R.id.creditsVLayout) as LinearLayout
                 for (card in cards) {
-                    val hlayout = LinearLayout(baseContext)
+                    val hlayout = LinearLayout(creditlayout.context)
                     hlayout.orientation = LinearLayout.HORIZONTAL
+                    hlayout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f)
                     val credittext = TextView(baseContext)
+                    credittext.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
                     credittext.setTextColor(Color.WHITE)
-                    credittext.gravity = Gravity.CENTER
+                    credittext.gravity = Gravity.CENTER_HORIZONTAL
                     credittext.setText(card.credittext)
                     hlayout.addView(credittext)
                     val crediturl = TextView(baseContext)
+                    crediturl.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
                     crediturl.setTextColor(Color.WHITE)
-                    crediturl.gravity = Gravity.CENTER
+                    crediturl.gravity = Gravity.CENTER_HORIZONTAL
                     crediturl.setText(card.crediturl)
                     Linkify.addLinks(crediturl, Linkify.WEB_URLS)
                     crediturl.setLinksClickable(true)
